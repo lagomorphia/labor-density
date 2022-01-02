85eaa50e7cd06a187acba9328c226798873df9f2
Tim Zhu <53026628+lodgepolepines@users.noreply.github.com>
2022-01-02 16:44:18 -0500

    add comments

--- app.R
+++ app.R
@@ -7,30 +7,25 @@ library(shiny)
 library(htmltools)
 library(spatialEco)
 
+# read in geospatial and csv data
 states <- readOGR(dsn="json/us-states.json")
 total <- read.csv("state_union_density.csv", check.names = FALSE)
 
+# transform csv data
 udata <- melt(total, id=c("state"))
-
 newobj <- merge(states, udata, by.x="name", by.y="state", duplicateGeoms = T)
 newobj <- newobj[,-c(2,3)]
 newobj$variable <- as.Date(newobj$variable, "%Y")
 newobj$variable <- format(newobj$variable, format="%Y")
 newobj$variable <- as.numeric(newobj$variable)
 newobj <- sp.na.omit(newobj, col.name="value", margin = 1)
+df <- as.data.frame(newobj)
+
+# add specs for viz
 bins <- c(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 100)
 pal <- colorBin("inferno", domain = newobj$value, bins = bins, reverse=TRUE)
 
-df <- as.data.frame(newobj)
-
-popup <- paste("<b>State: </b>", 
-               newobj$name,
-               "<br><b>Year: </b>", 
-               newobj$variable,
-               "<br><b>Union Density: </b>",
-               newobj$value) %>% 
-  lapply(htmltools::HTML)
-
+# create Shiny UI
 ui <- fluidPage(
   HTML("<h3>United States Labor Union Membership Density (1964-2018)</h3>"),
   mainPanel(
@@ -41,6 +36,7 @@ ui <- fluidPage(
     leafletOutput("mymap")
   ))
 
+# create Shiny server
 server <- function(input, output, session) {
   
   filteredData <- reactive({
